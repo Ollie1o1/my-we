8ecba8c67f1fcfbc3837bb8e7f50d44d8c29309b
Ollie1o1 <oliver.raczka.shue@gmail.com>
2024-09-26 17:05:59 -0400

    DIY_lab3.c

---
+++ DIY_lab3.c
@@ -0,0 +1,151 @@
+#include <stdio.h> 
+
+int main () {
+
+    printf("+"); // start of first line
+
+    for(int i =0; i < 10; i++)
+    {
+        printf("-");
+    }
+    printf("+");
+
+    for(int i =0; i < 10; i++)
+    {
+        printf("-");
+    }
+
+    printf("+\n");
+
+    printf("|"); // first line of collumns starts
+
+    for(int i = 0; i < 10; i++)
+    {
+        printf(" ");
+    }
+
+    printf("|");
+
+    for(int i =0; i < 10; i++)
+    {
+        printf(" ");
+    }
+
+    printf("|\n");   
+
+     printf("|");
+
+    for(int i = 0; i < 10; i++)
+    {
+        printf(" ");
+    }
+
+    printf("|");
+
+    for(int i =0; i < 10; i++)
+    {
+        printf(" ");
+    }
+
+    printf("|\n");   // second line of collumns
+
+     printf("|");
+
+    for(int i = 0; i < 10; i++)
+    {
+        printf(" ");
+    }
+
+    printf("|");
+
+    for(int i =0; i < 10; i++)
+    {
+        printf(" ");
+    }
+
+    printf("|\n");   // third line of collumns
+
+    // second set
+
+    printf("+"); // start of first line
+
+    for(int i =0; i < 10; i++)
+    {
+        printf("-");
+    }
+    printf("+");
+
+    for(int i =0; i < 10; i++)
+    {
+        printf("-");
+    }
+
+    printf("+\n");
+
+    printf("|");
+
+    for(int i = 0; i < 10; i++)
+    {
+        printf(" ");
+    }
+
+    printf("|");
+
+    for(int i =0; i < 10; i++)
+    {
+        printf(" ");
+    }
+
+    printf("|\n");   // fourth line of collumns
+
+     printf("|");
+
+    for(int i = 0; i < 10; i++)
+    {
+        printf(" ");
+    }
+
+    printf("|");
+
+    for(int i =0; i < 10; i++)
+    {
+        printf(" ");
+    }
+
+    printf("|\n");   // fifth line of collumns
+
+     printf("|");
+
+    for(int i = 0; i < 10; i++)
+    {
+        printf(" ");
+    }
+
+    printf("|");
+
+    for(int i =0; i < 10; i++)
+    {
+        printf(" ");
+    }
+
+    printf("|\n");   // sixth line of collumns
+
+    printf("+"); 
+
+    for(int i =0; i < 10; i++)
+    {
+        printf("-");
+    }
+    printf("+");
+
+    for(int i =0; i < 10; i++)
+    {
+        printf("-");
+    }
+
+    printf("+\n"); // last line
+
+    return 0;
+
+}
+
